1cffc557d18377052ea37384cd913d70377594f7
hellypatel026 <helly0026@gmail.com>
2026-03-24 19:13:57 +0530

    login css

--- classroom/classroom/settings.py
+++ classroom/classroom/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 AUTH_USER_MODEL = 'accounts.User'
 
-STATIC_URL = 'static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "classroom/static",
+]
