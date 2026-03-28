148f80d1772796fc361c9afae4069c14468e7f9b
hellypatel026 <helly0026@gmail.com>
2026-03-28 08:17:08 +0530

    doubt box css

--- classroom/courses/context_processors.py
+++ classroom/courses/context_processors.py
@@ -1,8 +1,37 @@
 from .models import Course
+from assignments.models import Assignment, Submission
+from django.utils import timezone
 
 def sidebar_courses(request):
+    courses = []
+    todos = []
+
     if request.user.is_authenticated:
-        return {
-            'courses': Course.objects.all()
-        }
-    return {}
+
+        # COURSES
+        if request.user.role == "student":
+            courses = Course.objects.filter(
+                enrollment__student=request.user
+            )
+        else:
+            courses = Course.objects.filter(
+                teacher=request.user
+            )
+
+        # SUBMITTED ASSIGNMENTS
+        submitted_ids = Submission.objects.filter(
+            student=request.user
+        ).values_list('assignment_id', flat=True)
+
+        # TODOS (ONLY PENDING + NOT EXPIRED)
+        todos = Assignment.objects.filter(
+            course__in=courses,
+            due_date__gte=timezone.now()
+        ).exclude(
+            id__in=submitted_ids
+        ).order_by('due_date')[:5]
+
+    return {
+        'courses': courses,
+        'todos': todos
+    }
